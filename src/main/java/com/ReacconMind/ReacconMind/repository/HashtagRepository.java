cf38a1f90364491b102fabe59bcc67b03c7b692d
Josue-Giron <jy.giron_dev@outlook.com>
2024-10-16 16:29:11 -0600

    Update branch Yared

--- src/main/java/com/ReacconMind/ReacconMind/repository/HashtagRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ReacconMind.ReacconMind.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ReacconMind.ReacconMind.model.Hashtag;
-
-public interface HashtagRepository extends JpaRepository <Hashtag, Integer> {
-    Hashtag findByName(String name);
-}
--- src/main/java/com/ReacconMind/ReacconMind/repository/TendencyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ReacconMind.ReacconMind.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ReacconMind.ReacconMind.model.Tendency;
-
-public interface TendencyRepository extends JpaRepository<Tendency, Integer> {
-    Tendency findByNameTendency(String nameTendency);
-}
